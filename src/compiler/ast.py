80e4a9225b36b99b2e348d9d661ac62d982f27ab
Sami Männistö <sami.mannisto1@gmail.com>
2025-01-20 09:22:28 +0200

    Add: base node classes for constructing AST

---
+++ src/compiler/ast.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class Expression:
+    """Base class for AST nodes representing expressions."""
+
+@dataclass
+class Literal(Expression):
+    """Class which represents a literal integer or boolean value"""
+    value: int | bool
+
+@dataclass
+class Identifier(Expression):
+    """Class which represents an identifier"""
+    name: str
+
+@dataclass
+class BinaryOp(Expression):
+    """AST node for a binary operation like `A + B`"""
+    left: Expression
+    op: str
+    right: Expression
