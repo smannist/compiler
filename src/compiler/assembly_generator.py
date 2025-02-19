0c9599d9b4f137cbd68ba532c84a713aabe5aead
Sami Männistö <sami.mannisto1@gmail.com>
2025-02-19 19:48:53 +0200

    Add: base assembly_generator

---
+++ src/compiler/assembly_generator.py
@@ -0,0 +1,45 @@
+import dataclasses
+from compiler import ir
+
+
+class Locals:
+    """Knows the memory location of every local variable."""
+    _var_to_location: dict[ir.IRVar, str]
+    _stack_used: int
+
+    def __init__(self, variables: list[ir.IRVar]) -> None:
+        self._var_to_location = {}
+        for index, var in enumerate(variables):
+            offset = 8 * (index + 1)
+            self._var_to_location[var] = f"-{offset}(%rbp)"
+        self._stack_used = 8 * len(variables)
+
+    def get_ref(self, v: ir.IRVar) -> str:
+        """Returns an Assembly reference like `-24(%rbp)`
+        for the memory location that stores the given variable"""
+        return self._var_to_location[v]
+
+    def stack_used(self) -> int:
+        """Returns the number of bytes of stack space needed for the local variables."""
+        return self._stack_used
+
+
+def get_all_ir_variables(instructions: list[ir.Instruction]) -> list[ir.IRVar]:
+    result_list: list[ir.IRVar] = []
+    result_set: set[ir.IRVar] = set()
+
+    def add(v: ir.IRVar) -> None:
+        if v not in result_set:
+            result_list.append(v)
+            result_set.add(v)
+
+    for insn in instructions:
+        for field in dataclasses.fields(insn):
+            value = getattr(insn, field.name)
+            if isinstance(value, ir.IRVar):
+                add(value)
+            elif isinstance(value, list):
+                for v in value:
+                    if isinstance(v, ir.IRVar):
+                        add(v)
+    return result_list
